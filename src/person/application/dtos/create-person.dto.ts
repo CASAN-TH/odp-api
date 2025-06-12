9eb371d4822cbb55797c2ccd87fbca7cfeb5320c
Nutkamon Manosatcharak <651435@live.hcu.ac.th>
2025-06-12 14:37:33 +0700

    [edit] example of DTO and entity

--- src/person/application/dtos/create-person.dto.ts
+++ src/person/application/dtos/create-person.dto.ts
@@ -5,6 +5,9 @@ export class CreatePersonDto {
   @ApiProperty({ description: 'Name of the person' })
   @IsString()
   name: string;
+  @ApiProperty({ description: 'Surname of the person' })
+  @IsString()
+  surname: string;
 
   companyId: string;
 }
